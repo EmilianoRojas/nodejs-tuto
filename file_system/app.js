b0c83f314cdb96e893e1358d1de9256159bf1e52
EmilianoRojas <inversiones.socony@gmail.com>
2020-02-08 19:21:31 -0300

    func

--- file_system/app.js
+++ file_system/app.js
@@ -1,1 +1,37 @@
 const fs = require('fs');
+
+console.log('Iniciado');
+
+/*
+fs.readFile('data.txt','utf-8',(error, data)=>{
+    if(error){
+        console.log(`Error ${error}`);
+    }else{
+        console.log(data)
+    }
+});
+*/
+
+let data = fs.readFileSync('data.txt','utf-8');
+console.log(data);
+
+console.log('Finalizado');
+
+
+/*
+fs.rename('data.txt', 'data_renombrado.txt', (error)=>{
+    if(error) throw error;
+    console.log('Renombrado');
+})
+*/
+
+fs.appendFile('data.txt', '\n gracias por dar pepa', (error)=>{
+    if(error) console.log(`Error${error}`);
+    
+});
+
+fs.unlink('data2.txt',(error)=>{
+    if(error) throw error;
+    console.log('ELiminado');
+})
+
